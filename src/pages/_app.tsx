e1d9d4d2d245e65bfc50d172bf23dd9c60d75edc
Darshan khokhariya <120724017+Darshankhokhariya@users.noreply.github.com>
2025-10-07 10:25:28 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,7 +27,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
                 <title>{pageMetadata.title}</title>
                 <meta name="description" content={pageMetadata.description} />
                 <meta name="keywords" content={pageMetadata.keywords.join(",")} />
-
+<meta name="google-site-verification" content="eEsDPrArudUceYMlgIY4I0a-ZaBBiBGwMxvvZQRiR7o" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
